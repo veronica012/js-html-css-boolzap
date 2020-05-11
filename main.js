8874b5eb6081631770170055f1ed4a9fca4c0be5
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-11 13:29:26 +0200

    funzione settimeout milestone2 prima parte da rivedere

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ $('.fa-microphone').click(function(){
     var testo_utente = $('#scrivi_messaggio').val();
     console.log(testo_utente);
     //attraverso la funzione append creo un nuovo elemento all'interno dell'html che contiene il testo scritto dall'utente in input
-    // $('.container-chat').append('<div class="messaggio risposta">' + testo_utente + '</div>');
+    // $('.container-chat').append('<div class="messaggio-ricevuto">' + testo_utente + '</div>');
 
+    //alternativamente creare un template in html (css display: none)
     var nuovo_messaggio = $('.template .messaggio-ricevuto').clone();
     nuovo_messaggio.text(testo_utente);
     $('.container-chat').append(nuovo_messaggio);
-    //alternativamente creare un template in html (css display: none)
+
+    var risposta = 'ok';
+    setTimeout(myFunction, 2000);
+    
+function myFunction() {
+    var risposta_interlocutore = $('.template .messaggio').clone();
+    risposta_interlocutore.text(risposta);
+    $('.container-chat').append(risposta_interlocutore);
+}
+
+
+
 
 });
