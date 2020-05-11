8354797f6a5c946b6c46f6cffe921f0007e39e1d
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-11 12:13:51 +0200

    aggiunti commenti

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+//intercetto il click sull'icona che permette di inviare il messaggio
 $('.fa-microphone').click(function(){
+    //creo la variabile per salvare il risultato di della funzione .val
     var testo_utente = $('#scrivi_messaggio').val();
     console.log(testo_utente);
+    //attraverso la funzione append creo un nuovo elemento all'interno dell'html che contiene il testo scritto dall'utente in input
     // $('.container-chat').append('<div class="messaggio risposta">' + testo_utente + '</div>');
 
     var nuovo_messaggio = $('.template .messaggio-ricevuto').clone();
