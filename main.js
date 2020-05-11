9299b69d6a24a317ff9c60996b4cd2b72ea1b2ed
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-11 11:41:36 +0200

    nomi delle classi modificati per usare il template

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $('.fa-microphone').click(function(){
     console.log(testo_utente);
     // $('.container-chat').append('<div class="messaggio risposta">' + testo_utente + '</div>');
 
-    var nuovo_messaggio = $('.template .messaggio').clone();
+    var nuovo_messaggio = $('.template .messaggio-ricevuto').clone();
     nuovo_messaggio.text(testo_utente);
     $('.container-chat').append(nuovo_messaggio);
     //alternativamente creare un template in html (css display: none)
