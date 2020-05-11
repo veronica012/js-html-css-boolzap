a3c3fa3fa7adb50b013214925d6d2c872d42310a
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-11 11:34:42 +0200

    milestone 1 template

---
+++ main.js
@@ -0,0 +1,11 @@
+$('.fa-microphone').click(function(){
+    var testo_utente = $('#scrivi_messaggio').val();
+    console.log(testo_utente);
+    // $('.container-chat').append('<div class="messaggio risposta">' + testo_utente + '</div>');
+
+    var nuovo_messaggio = $('.template .messaggio').clone();
+    nuovo_messaggio.text(testo_utente);
+    $('.container-chat').append(nuovo_messaggio);
+    //alternativamente creare un template in html (css display: none)
+
+});
